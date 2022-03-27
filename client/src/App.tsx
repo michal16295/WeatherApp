29672c3d6532691ab18519085483311d75ed6ff8
michal16295 <michal16292@gmail.com>
2022-03-27 15:20:58 +0300

    added error boundery

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,22 @@
 import React from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import ErrorBoundary from "./components/ErrorBoundary";
+
+import Home from "./screens/Home";
+
+import routes from "./routes.json";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const publicRoutes = [
+    <Route key="Home" path={routes.HOME} element={<Home />} />,
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary>
+      <Router></Router>
+    </ErrorBoundary>
   );
-}
+};
 
 export default App;

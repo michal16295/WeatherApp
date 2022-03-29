e326362e89aeb0ccc6a7736ba5feef4b4fe0971b
michal16295 <michal16292@gmail.com>
2022-03-29 14:24:48 +0300

    info ui

--- client/src/components/Home/WeatherData.tsx
+++ client/src/components/Home/WeatherData.tsx
@@ -5,9 +5,19 @@ const WeatherData = () => {
   return (
     <Container>
       <Box>
-        <Image>
-          <img src="https://res.cloudinary.com/photocloudmichal/image/upload/v1648477033/weather/sun/8_acbu1p.png" />
-        </Image>
+        <Row>
+          <div>
+            <Image>
+              <img src="https://res.cloudinary.com/photocloudmichal/image/upload/v1648477033/weather/sun/8_acbu1p.png" />
+            </Image>
+            <h2>Heavy Rain</h2>
+            <h3>Tonight</h3>
+          </div>
+          <Info>
+            <div>23°</div>
+            <p>Feels like 32°</p>
+          </Info>
+        </Row>
       </Box>
     </Container>
   );
@@ -22,7 +32,7 @@ const Container = styled.div`
 `;
 
 const Box = styled.div`
-  border-radius: 12px;
+  border-radius: 20px;
   width: 100%;
   background: linear-gradient(
     110deg,
@@ -30,17 +40,48 @@ const Box = styled.div`
     rgba(87, 151, 252, 1) 100%
   );
   min-height: 200px;
-  box-shadow: 2px 6px 6px rgb(191, 215, 252);
+  box-shadow: 3px 7px 8px 1px rgb(191, 215, 252);
   padding: 10px 15px;
+  color: #fdfefe;
+
+  h2 {
+    line-height: 1px;
+    font-size: clamp(1.4rem, 4vw, 1.5rem);
+  }
+  h3 {
+    font-size: clamp(1rem, 4vw, 1.1rem);
+    line-height: 10px;
+  }
+`;
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Image = styled.div`
   width: 150px;
   height: auto;
-  margin-top: -30px;
+  margin-top: -45px;
 
   img {
     height: 100%;
     width: 100%;
+  }
+`;
+
+const Info = styled.div`
+  div {
+    font-size: 72px;
+    background: -webkit-linear-gradient(#eee, #a7c8fd);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-weight: 600;
+  }
+  p {
+    color: #ecf3fd;
+    font-weight: 600;
+    line-height: 1px;
+    font-size: clamp(1rem, 4vw, 1.2rem);
   }
 `;
